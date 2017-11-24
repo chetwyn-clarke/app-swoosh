b4168dc06d8b380bb7e43dabaa70712329d80b69
chetwyn-clarke <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2017-11-24 13:25:59 -0500

    finished lesson 2

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var getStartedBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
+        
+        bgImg.frame = view.frame
+        
+        getStartedBtn.frame = CGRect(x: view.frame.size.width / 2 - getStartedBtn.frame.size.width / 2, y: view.frame.size.height - 75, width: getStartedBtn.frame.size.width, height: getStartedBtn.frame.size.height)
+        
     }
 
     override func didReceiveMemoryWarning() {
