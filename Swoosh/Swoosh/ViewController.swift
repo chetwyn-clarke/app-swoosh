6f6d4d41193e011372f003802c2f6eefcdd45df6
chetwyn-clarke <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2017-11-25 23:13:25 -0500

    finished lesson 3

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -17,12 +17,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
-        
-        bgImg.frame = view.frame
-        
-        getStartedBtn.frame = CGRect(x: view.frame.size.width / 2 - getStartedBtn.frame.size.width / 2, y: view.frame.size.height - 75, width: getStartedBtn.frame.size.width, height: getStartedBtn.frame.size.height)
-        
     }
 
     override func didReceiveMemoryWarning() {
