e0b305e868455e9c629578c548451142ad5ae733
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2017-11-27 10:54:14 -0500

    Finished lesson 08

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -10,17 +10,17 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onNextBtnTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toSkillVC", sender: self)
     }
-    
 
     /*
     // MARK: - Navigation
